3bc12f558f5094741fa98432b694876058703095
James Barker <james.barker@esendex.com>
2017-03-18 17:17:12 +0000

    Loads from data source

--- Web/DataDisplay/Models/HeatmapData.cs
+++ Web/DataDisplay/Models/HeatmapData.cs
@@ -8,11 +8,21 @@ namespace DataDisplay.Models
 
         public double[][] ToSimpleHeatData(double width, double height)
         {
+            // width = 600
+            // height = 600
+
+            // input = 0, output = 300
+
+            var valanceRange = 20;
+            var arousalRange = 18;
+
+            // arousal
+
             return DataPoints
                 .Select(point => new[]
                 {
-                    width + (point.Valence * width),
-                    height + (point.Arousal * height),
+                    ((point.Valence + valanceRange) / (valanceRange * 2)) * width,
+                    height - ((point.Arousal / arousalRange) * height),
                     1
                 })
                 .ToArray();
